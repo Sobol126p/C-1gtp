7ee63b5d059f4c85b6b4af8a782c4215a88a7a97
Sobol126p <62239051+Sobol126p@users.noreply.github.com>
2020-11-12 11:25:08 +0100

    Update lekcja4_kolory.cpp

--- lekcja4_kolory.cpp
+++ lekcja4_kolory.cpp
@@ -7,13 +7,16 @@ using namespace std;
 int main(int argc, char** argv) {
 	int kolor = 0x00123456;
 	int red = 0x00ff0000;
+	int red2 = (kolor & red);
 	int green = 0x0000ff00;
+	int green2 = (kolor & green);
 	int blue = 0x000000ff;
+	int blue2 = (kolor & blue);
 	int przezr = 0xff000000;
-	cout<<"Czerwony: "<<(kolor & red)<<endl;
-	cout<<"Zielony: "<<(kolor & green)<<endl;
-	cout<<"Niebieski: "<<(kolor & blue)<<endl;
-	cout<<"Przezroczystosc: "<<(kolor & przezr)<<endl;
+	cout<<"Czerwony: "<<hex<<(red2>>16)<<"    "<<dec<<(red2>>16)<<endl;
+	cout<<"Zielony: "<<hex<<(green2>>8)<<"    "<<dec<<(green2>>8)<<endl;
+	cout<<"Niebieski: "<<hex<<(blue2)<<"    "<<dec<<(blue2)<<endl;
+	cout<<"Przezroczystosc: "<<hex<<(kolor & przezr)<<"    "<<dec<<(kolor & przezr)<<endl;
 	
 	return 0;
 }
