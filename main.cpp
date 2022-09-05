50de06056e2ed36f8758f29645863a3031d1dd7f
Sobol126p <62239051+Sobol126p@users.noreply.github.com>
2022-09-05 14:15:22 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,45 @@
+#include <cstdlib>
 #include <iostream>
-#include <fstream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 
-ifstream plik;
-ifstream plik1;
-ofstream wynik;
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char** argv) {
-plik.open("C:\\Users\\48535\\Desktop\\liczby1.txt");
-plik1.open("C:\\Users\\48535\\Desktop\\liczby2.txt");
-wynik.open("C:\\Users\\48535\\Desktop\\wynik.txt");
-	int liczba;
-	int liczba1;
-	int wieksze;
-	int rowne;
-	 if(plik.good()&&plik.good())  //sprawdzenie czy plik istnieje
-     while(!plik.eof()&&!plik1.eof())        //petla wykonuje sie az program dojedzie do konca pliku
-     {
-     	plik>>liczba;
-     	plik1>>liczba1;
-     	if(liczba>liczba1)
-     	{
-     		wieksze++;
-		 }
-        if(liczba==liczba1)
-        {
-        	rowne++;
-	}
-     }
-     wynik<<"Liczby wiêksze: "<<wieksze; wynik<<"  Liczby równe: "<<rowne;
-     cout<<"Liczby wiêksze: "<<wieksze; cout<<"  Liczby równe: "<<rowne;
-	return 0;
+int wybor,i;
+float f;
+
+    printf("Menu: \n");
+
+    printf("1 - ZaokrÄ…glij liczby \n");
+    
+    printf("2 - System szesnastkowy \n");
+    
+    printf("3 - Zera \n");
+    
+    printf("4 - Koniec \n");
+    
+    scanf("%d", &wybor);
+    
+    switch(wybor)
+    {
+        case 1:
+            printf("Podaj liczbÄ™ zmiennoprzecinkowa: \n");
+            scanf("%f", &f);
+            printf("ZaokrÄ…glij cyfry: %.2f \n", f );
+        break;
+        case 2:
+            printf("Podaj liczbÄ™: \n");
+            scanf("%i", &i);
+            printf("System szesnastkowy: %x \n", i );
+        break;
+        case 3:
+            printf("Podaj liczbÄ™: \n");
+            scanf("%i", &i);
+            printf("Zera: %07d", i );
+        break;
+        case 4:
+            exit;
+        break;
+    }
 }
