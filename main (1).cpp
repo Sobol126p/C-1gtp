3c0b035f1f2bfaf9d7677ccdb07b5dc957b5ca3a
Sobol126p <62239051+Sobol126p@users.noreply.github.com>
2022-09-05 14:21:07 +0200

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,45 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+int wybor,i;
+float f;
+
+    printf("Menu: \n");
+
+    printf("1 - Zaokrąglij liczby \n");
+    
+    printf("2 - System szesnastkowy \n");
+    
+    printf("3 - Zera \n");
+    
+    printf("4 - Koniec \n");
+    
+    scanf("%d", &wybor);
+    
+    switch(wybor)
+    {
+        case 1:
+            printf("Podaj liczbę zmiennoprzecinkową: \n");
+            scanf("%f", &f);
+            printf("Zaokrąglij cyfry: %.2f \n", f );
+        break;
+        case 2:
+            printf("Podaj liczbę: \n");
+            scanf("%i", &i);
+            printf("System szesnastkowy: %x \n", i );
+        break;
+        case 3:
+            printf("Podaj liczbę: \n");
+            scanf("%i", &i);
+            printf("Zera: %06d", i );
+        break;
+        case 4:
+            exit;
+        break;
+    }
+}
